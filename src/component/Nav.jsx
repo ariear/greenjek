2696b4faef02862bf931d21fccb89c40401221f3
arie <arieinjector@gmail.com>
2022-04-22 21:20:50 +0700

    add menu

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import OutsideClickHandler from 'react-outside-click-handler';
 
 function Nav() {
+    const [open, setOpen] = useState(false)
+
     return (
     <nav className="flex items-center justify-between py-4 px-5 md:px-10 shadow-md fixed w-screen bg-white z-50">
         <img className="w-[60px] lg:w-[80px]" src="/img/logo-jek.png" alt="dsadas" />
@@ -13,7 +17,25 @@ function Nav() {
         </div>
         <div className="flex items-center">
         <button className="py-2 px-5 md:px-8 rounded-lg text-white font-jaksel bg-[#5CB30B] shadow-md">Sign Up</button>
+        <OutsideClickHandler 
+            onOutsideClick={() => {
+                setOpen(false)
+            }}
+        >
+        <button className="lg:hidden block" onClick={() => setOpen(!open)}>
         <img src="/img/menu-jek.svg" className="lg:hidden block ml-5" alt="das" />
+        </button>
+        {
+            open && 
+            <div className="fixed lg:hidden bg-white list-none font-dmsans text-center rounded-md shadow-lg top-20 right-8">
+                <li className="border-b px-6 py-2"><Link to="/">Home</Link></li>
+                <li className="border-b px-6 py-2"><Link to="/service">Our Service</Link></li>
+                <li className="border-b px-6 py-2">About US</li>
+                <li className="border-b px-6 py-2">Advantages</li>
+                <li className="px-6 py-2">Our Team</li>
+            </div>
+        }
+        </OutsideClickHandler>
         </div>
     </nav>
     )
